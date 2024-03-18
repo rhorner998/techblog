0d1eb2e59cae0b3822912efc7e02689ef184c753
unknown <russellstephen16@gmail.com>
2024-03-18 16:05:25 -0500

    still trying to fix dashboard 3

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -2,21 +2,23 @@
 
 // Middleware to check if the user is authenticated
 const ensureAuthenticated = (req, res, next) => {
-    if (req.session.userId) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  };
-  
-  // Middleware to prevent logged-in users from accessing certain routes
-  const redirectIfAuthenticated = (req, res, next) => {
-    if (req.session.userId) {
-      res.redirect('/dashboard');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = { ensureAuthenticated, redirectIfAuthenticated };
-  
+  if (req.session.userId) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// Middleware to redirect authenticated users to the dashboard
+const redirectIfAuthenticated = (req, res, next) => {
+  if (req.session.userId) {
+      // Specify the routes that should not be redirected to the dashboard
+      const allowedRoutes = ['/dashboard', '/logout']; // Add more routes if needed
+      if (!allowedRoutes.includes(req.path)) {
+          return res.redirect('/dashboard');
+      }
+  }
+  next();
+};
+
+module.exports = { ensureAuthenticated, redirectIfAuthenticated };
